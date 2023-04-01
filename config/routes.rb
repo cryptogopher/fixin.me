0e722193733488b1b959ce7c417b4402a9f06b95
cryptogopher <cryptogopher@noreply.me>
2023-04-01 18:53:30 +0200

    Require authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {registration: 'register'}
+
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "users#index"
 end
