353049a6b24feac9f7ff0c1fa6b2ccb305248352
cryptogopher <cryptogopher@noreply.me>
2023-04-14 21:35:32 +0200

    Set User :confirmable
    Renumber statuses to make :disabled == 0

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
 
   # Statuses ordered by decreasing privileges
   enum :status, {
-    admin: 1,       # admin level access
-    active: 2,      # read-write user level access
-    restricted: 3,  # read-only user level access
-    locked: 4,      # disallowed to sign in due to failed logins
-    disabled: 5     # administratively disallowed to sign in
+    admin: 4,       # admin level access
+    active: 3,      # read-write user level access
+    restricted: 2,  # read-only user level access
+    locked: 1,      # disallowed to sign in due to failed logins; maintained by Devise :lockable
+    disabled: 0,    # administratively disallowed to sign in
   }, default: :active
 end
