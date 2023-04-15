6d71d3868083ad342ea5291a4c4e858135cf447a
cryptogopher <cryptogopher@noreply.me>
2023-04-15 23:52:02 +0200

    Add registration redirection and mail confirmation sending

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {registration: 'register'}
+  devise_for :users, path: '', path_names: {registration: 'register'},
+    controllers: {registrations: :registrations}
 
   resources :users
 
