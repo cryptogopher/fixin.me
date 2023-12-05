588bc019113187db5f2a06dd34fc1cc6f3c34318
cryptogopher <cryptogopher@noreply.me>
2023-12-05 23:36:56 +0100

    Fill <td> with <a> link using ::after pseudoelement

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -88,11 +88,6 @@ module ApplicationHelper
     image_element_to(:link, name, image, options, html_options)
   end
 
-  # Workaround to fill <td> with link completely, see CSS for details.
-  def table_link_to(...)
-    link_to(...) * 2
-  end
-
 
   private
 
