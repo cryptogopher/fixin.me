c96b64608776175881d7019be44b6ceb4c26abcf
cryptogopher <cryptogopher@noreply.me>
2024-01-04 23:06:47 +0100

    Hide page content until turbo:load event
    To avoid cases when Turbo links are processed as normal HTTP requests

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 Turbo.session.drive = false
+
+function showPage(event) {
+  document.documentElement.style.visibility="visible";
+}
+document.addEventListener('turbo:load', showPage);
