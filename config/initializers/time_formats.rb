ba3c2a99b9e8483dfcc6ebc1696633a4221be1e7
cryptogopher <cryptogopher@noreply.me>
2024-01-06 03:49:18 +0100

    Don't trigger file encoding detection

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,2 +1,3 @@
-# Format contains non-breaking space: 160.chr(Encoding::UTF_8)
+# Format contains non-breaking space:
+#   160.chr(Encoding::UTF_8)
 Time::DATE_FORMATS[:db_without_sec] = "%Y-%m-%d %H:%M"
