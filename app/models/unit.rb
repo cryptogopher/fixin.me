a5a75233c70681c1c18f349bea9f882653aa4fe7
cryptogopher <cryptogopher@noreply.me>
2024-04-02 22:20:45 +0200

    Validate Unit base user

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,17 +1,17 @@
 class Unit < ApplicationRecord
   belongs_to :user, optional: true
-  # TODO: validate base.user == user
   belongs_to :base, optional: true, class_name: "Unit"
   has_many :subunits, class_name: "Unit", dependent: :restrict_with_error, inverse_of: :base
 
+  validate if: ->{ base.present? } do
+    errors.add(:base, :user_mismatch) unless user == base.user
+    errors.add(:base, :multilevel_nesting) if base.base.present?
+  end
   validates :symbol, presence: true, uniqueness: {scope: :user_id},
     length: {maximum: columns_hash['symbol'].limit}
   validates :name, length: {maximum: columns_hash['name'].limit}
   validates :multiplier, numericality: {equal_to: 1}, unless: :base
   validates :multiplier, numericality: {other_than: 0}, if: :base
-  validate if: ->{ base&.base.present? } do
-    errors.add(:base, :multilevel_nesting)
-  end
 
   scope :defaults, ->{ where(user: nil) }
   scope :ordered, ->{
