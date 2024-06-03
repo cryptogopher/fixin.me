769e4af6031eef7d63e172b911fc91f71d2b4215
cryptogopher <cryptogopher@noreply.me>
2024-06-04 00:47:50 +0200

    Bring per-installation setting to application.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'fixinme@noreply.me'
+  # This is set in 'config/application.rb'.
+  #config.mailer_sender = 'fixinme@noreply.me'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
