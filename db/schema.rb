a18f257378ec3d09a0517058b1760b9d52171e22
cryptogopher <cryptogopher@noreply.me>
2024-12-08 15:28:38 +0100

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_06_02_185352) do
+ActiveRecord::Schema[7.2].define(version: 2023_06_02_185352) do
   create_table "units", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
-    t.string "symbol"
-    t.string "name"
-    t.decimal "multiplier", precision: 30, scale: 15, default: "1.0"
+    t.string "symbol", limit: 15, null: false
+    t.text "description"
+    t.decimal "multiplier", precision: 30, scale: 15, default: "1.0", null: false
     t.bigint "base_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
