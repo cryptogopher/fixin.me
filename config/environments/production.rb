c4def9cd860b2ae6a11892ab9935bbeff0c0a372
cryptogopher <cryptogopher@noreply.me>
2026-04-30 18:26:43 +0200

    Filter :email field logging only in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Protect e-mail addresses from being logged only in production.
+  config.filter_parameters += [:email]
 end
